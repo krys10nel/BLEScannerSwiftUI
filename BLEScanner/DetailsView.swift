4e2271360df2641a8493722413a4643af359b353
Krystene <maceda.kn@gmail.com>
2024-06-07 10:48:29 -0400

    edited LightDisclosureStyle, turned collapsible area into a VStack instead of button to prevent accidental collapsing of category when attempting to click the buttons

--- BLEScanner/DetailsView.swift
+++ BLEScanner/DetailsView.swift
@@ -158,16 +158,6 @@ struct DetailsView: View {
                     }
                     
                     if let landingLightsService = device.discoveredServices.first(where: {device.knownServiceNames[$0.uuid.uuidString] == "Landing Lights"}) {
-                        /*
-                         VStack(alignment: .leading) {
-                            Text("Landing Lights")
-                                .frame(minWidth: 111, idealWidth: .infinity, maxWidth: .infinity, alignment: .topLeading)
-                                .foregroundStyle(.red)
-                                .lineLimit(1)
-                            LightGroupView(device: device, service: landingLightsService, proxy: geo)
-                        }
-                        .padding()
-                         */
                         DisclosureGroup {
                             VStack(alignment: .leading) {
                                 LightGroupView(device: device, service: landingLightsService, proxy: geo)
@@ -182,16 +172,6 @@ struct DetailsView: View {
                     }
                     
                     if let antiCollisionLightsService = device.discoveredServices.first(where: {device.knownServiceNames[$0.uuid.uuidString] == "Anti-Collision Lights"}) {
-                        /*
-                         VStack(alignment: .leading) {
-                            Text("Anti-Collision Lights")
-                                .frame(minWidth: 111, idealWidth: .infinity, maxWidth: .infinity, alignment: .topLeading)
-                                .foregroundStyle(.red)
-                                .lineLimit(1)
-                            LightGroupView(device: device, service: antiCollisionLightsService, proxy: geo)
-                        }
-                        .padding()
-                         */
                         DisclosureGroup {
                             VStack(alignment: .leading) {
                                 LightGroupView(device: device, service: antiCollisionLightsService, proxy: geo)
@@ -228,16 +208,6 @@ struct DetailsView: View {
                         }
                         
                         if let landingLightsService = device.discoveredServices.first(where: {device.knownServiceNames[$0.uuid.uuidString] == "Landing Lights"}) {
-                            /*
-                             VStack(alignment: .leading) {
-                                Text("Landing Lights")
-                                    .frame(minWidth: 111, idealWidth: .infinity, maxWidth: .infinity, alignment: .topLeading)
-                                    .foregroundStyle(.red)
-                                    .lineLimit(1)
-                                LightGroupView(device: device, service: landingLightsService, proxy: geo)
-                            }
-                            .padding()
-                             */
                             DisclosureGroup {
                                 VStack(alignment: .leading) {
                                     LightGroupView(device: device, service: landingLightsService, proxy: geo)
@@ -252,16 +222,6 @@ struct DetailsView: View {
                         }
                         
                         if let antiCollisionLightsService = device.discoveredServices.first(where: {device.knownServiceNames[$0.uuid.uuidString] == "Anti-Collision Lights"}) {
-                            /*
-                             VStack(alignment: .leading) {
-                                Text("Anti-Collision Lights")
-                                    .frame(minWidth: 111, idealWidth: .infinity, maxWidth: .infinity, alignment: .topLeading)
-                                    .foregroundStyle(.red)
-                                    .lineLimit(1)
-                                LightGroupView(device: device, service: antiCollisionLightsService, proxy: geo)
-                            }
-                            .padding()
-                             */
                             DisclosureGroup {
                                 VStack(alignment: .leading) {
                                     LightGroupView(device: device, service: antiCollisionLightsService, proxy: geo)
@@ -329,72 +289,72 @@ struct LightGroupView: View {
     
     var body: some View {
         HStack {
-            //ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: proxy.size.width * 0.25))], alignment: .leading, spacing: 5) {
-                    ForEach(device.discoveredCharacteristics, id: \.uuid) { characteristic in
-                        if characteristic.service.uuid == service.uuid {
-                            let properties = characteristic.characteristic.properties
-                            if properties.contains(.write) {
-                                HStack {
-                                    // TODO: while characteristic name == previous characteristic name
-                                    VStack {
-                                        Spacer()
-                                        Text("\(characteristic.characteristicName)")
-                                            .frame(idealWidth: .infinity, maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                                            .font(.caption)
-                                            .foregroundStyle(.gray)
-                                            .multilineTextAlignment(.center)
-                                            .lineLimit(2)
-                                        Spacer()
-                                        Text("\(characteristic.description)")
-                                            .frame(idealWidth: .infinity, maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                                            .foregroundStyle(.white)
-                                            .lineLimit(1)
-                                        Spacer()
-                                        Button(action: {
-                                            print("Clicked on \(characteristic)")
-                                            self.device.toggleCharacteristic(characteristic: characteristic)
-                                        }) {
-                                            Image(systemName: "power.circle")
-                                                .font(.system(size: 50))
-                                                .foregroundStyle(characteristic.readValue == "01" ? Color.green : Color.gray)
-                                        }
-                                        .buttonStyle(.bordered)
-                                        Spacer()
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: proxy.size.width * 0.25))], alignment: .leading, spacing: 5) {
+                ForEach(device.discoveredCharacteristics, id: \.uuid) { characteristic in
+                    if characteristic.service.uuid == service.uuid {
+                        let properties = characteristic.characteristic.properties
+                        if properties.contains(.write) {
+                            HStack {
+                                VStack {
+                                    Spacer()
+                                    Text("\(characteristic.characteristicName)")
+                                        .frame(idealWidth: .infinity, maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                        .font(.caption)
+                                        .foregroundStyle(.gray)
+                                        .multilineTextAlignment(.center)
+                                        .lineLimit(2)
+                                    Spacer()
+                                    Text("\(characteristic.description)")
+                                        .frame(idealWidth: .infinity, maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                                        .foregroundStyle(.white)
+                                        .lineLimit(1)
+                                    Spacer()
+                                    Button(action: {
+                                        print("Clicked on \(characteristic)")
+                                        self.device.toggleCharacteristic(characteristic: characteristic)
+                                        
+                                        // TODO: while characteristic name == previous characteristic name
+                                    }) {
+                                        Image(systemName: "power.circle")
+                                            .font(.system(size: 50))
+                                            .foregroundStyle(characteristic.readValue == "01" ? Color.green : Color.gray)
                                     }
-                                    .frame(width: proxy.size.width * 0.25, alignment: .topLeading)
+                                    .buttonStyle(.bordered)
+                                    Spacer()
                                 }
+                                .frame(width: proxy.size.width * 0.25, alignment: .topLeading)
                             }
                         }
                     }
                 }
-            //}
-            //.frame(width: proxy.size.width, alignment: .topLeading)
+            }
         }
     }
 }
 
 @available(iOS 16.0, *)
 struct LightDisclosureStyle: DisclosureGroupStyle {
-    
-    // TODO: when the configuration is expanded, the whole area is a button. Fix so only text area is a button for collapsing
     func makeBody(configuration: Configuration) -> some View {
         VStack {
-            Button {
-                configuration.isExpanded.toggle()
-            } label: {
-                HStack(alignment: .firstTextBaseline) {
-                    configuration.label.padding(.leading, 0)
-                    Image(systemName: configuration.isExpanded ? "chevron.down" : "chevron.right")
-                        .foregroundStyle(.red)
-                        .font(.body)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                        .animation(nil, value: configuration.isExpanded)
-                }
-                .contentShape(Rectangle())
+            HStack(alignment: .firstTextBaseline) {
+                configuration.label
+                    .padding([.top, .bottom], 10)
+                    .lineLimit(1)
+                    .layoutPriority(1)
+                Image(systemName: configuration.isExpanded ? "chevron.down" : "chevron.right")
+                    .foregroundStyle(.red)
+                    .font(.body)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .animation(.easeInOut(duration: 0.3), value: configuration.isExpanded)
             }
+            .onTapGesture {
+                configuration.isExpanded.toggle()
+            }
+            .contentShape(Rectangle())
+            
             if configuration.isExpanded {
-                configuration.content.padding(.leading, 0)
+                configuration.content
+                    .padding(.leading, 0)
             }
         }
     }
